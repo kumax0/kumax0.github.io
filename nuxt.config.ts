840bbae08e7d2fcee4c02515ce16a614c097b9f5
Caner Ünüs <caner.uenues@brandad-systems.de>
2023-04-12 22:09:17 +0200

    inital commit

---
+++ nuxt.config.ts
@@ -0,0 +1,4 @@
+// nuxt.config.js
+export default defineNuxtConfig ({
+    modules: ['@nuxtjs/tailwindcss']
+})
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './pages/**/*.vue',
+    './components/**/*.vue',
+    './layouts/**/*.vue',
+    './plugins/**/*.js',
+    './nuxt.config.js',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
